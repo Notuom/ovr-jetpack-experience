c4a1ba07a51efef0c26bfdffe27e30b8e6cd3a17
Notuom <thibault.karl@gmail.com>
2017-11-02 16:57:59 -0400

    Add textures and terrain, bad collider and try something for jetpack stuff

--- Assets/_Scripts/OculusTouchInteractions.cs
+++ Assets/_Scripts/OculusTouchInteractions.cs
@@ -14,21 +14,31 @@ public class OculusTouchInteractions : MonoBehaviour
     private Rigidbody _rb;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.rotation = CameraGameObject.transform.rotation;
-        float leftTriggerInput = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
-        float rightTriggerInput = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
-        if (leftTriggerInput > 0 || rightTriggerInput > 0)
+        var leftTriggerInput = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, LeftController);
+        var rightTriggerInput = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, RightController);
+        if (leftTriggerInput > 0)
         {
-            Debug.Log("Trigger input! L" + leftTriggerInput + ", R" + rightTriggerInput + ", Z Rotation: " + transform.rotation.z);
-            _rb.AddForce(0, Math.Max(leftTriggerInput, rightTriggerInput) * ThrustSpeed, 0);
+            var leftControllerRotation = OVRInput.GetLocalControllerRotation(LeftController);
+            Debug.Log("LEFT Input! I:" + leftTriggerInput + ", R:" + leftControllerRotation);
+            _rb.AddForce(leftControllerRotation * Vector3.one * ThrustSpeed);
+//            Debug.DrawLine(Vector3.zero, leftControllerRotation * Vector3.one);
+//            _rb.AddForce(0, Math.Max(leftTriggerInput, rightTriggerInput) * ThrustSpeed, 0);
+        }
+        else if (rightTriggerInput > 0)
+        {
+            var rightControllerRotation = OVRInput.GetLocalControllerRotation(RightController);
+            Debug.Log("RIGHT Input! I:" + rightTriggerInput + ", R:" + rightControllerRotation);
+//            Debug.DrawLine(Vector3.zero, rightControllerRotation * Vector3.one);
+//            _rb.AddForce(0, Math.Max(rightTriggerInput, rightTriggerInput) * ThrustSpeed, 0);
         }
     }
 }
