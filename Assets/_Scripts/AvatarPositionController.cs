1d2fd0874ce583996a2de162e8acec15833d0df5
Notuom <thibault.karl@gmail.com>
2017-11-03 22:00:23 -0400

    Realign character

--- Assets/_Scripts/AvatarPositionController.cs
+++ Assets/_Scripts/AvatarPositionController.cs
@@ -7,6 +7,10 @@ public class AvatarPositionController : MonoBehaviour
     private void Update()
     {
         var anchorPosition = AnchorObject.transform.position;
-        transform.position = new Vector3(anchorPosition.x, anchorPosition.y - 1.5f, anchorPosition.z - 0.2f);
+        transform.position = new Vector3(anchorPosition.x, anchorPosition.y - 1.5f, anchorPosition.z);
+        var anchorAngles = AnchorObject.transform.rotation.eulerAngles;
+        var eulerAngles = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(eulerAngles.x, anchorAngles.y, eulerAngles.z);
+//        transform.forward = new Vector3(transform.forward.x, transform.forward.y, AnchorObject.transform.forward.z);
     }
 }
